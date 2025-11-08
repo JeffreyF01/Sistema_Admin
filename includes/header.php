614c2dc1a3b018730f0763b4a0caf4a3432314c9
Guaca27 <reyesguaca@gmail.com>
2025-11-08 01:56:30 -0400

    Cambios en el header

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Ventas - UTESA</title>
+    <title>Sistema de Ventas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="/Sistema_Admin/assets/css/style.css" rel="stylesheet">
     <link href="/Sistema_Admin/assets/css/sidebar.css" rel="stylesheet">
