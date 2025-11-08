7cbe3b39c5c06cb36c960e8ed9eb0d106be9cac6
Guaca27 <reyesguaca@gmail.com>
2025-11-08 01:50:50 -0400

    modifique los tamanos de los contenedores

--- includes/header.php
+++ includes/header.php
@@ -7,11 +7,22 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="/Sistema_Admin/assets/css/style.css" rel="stylesheet">
+    <link href="/Sistema_Admin/assets/css/sidebar.css" rel="stylesheet">
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    
+    <style>
+        body {
+            overflow-x: hidden;
+        }
+        .app-container {
+            display: flex;
+            min-height: 100vh;
+        }
+    </style>
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
